4e7e39bc7db074fa5bc5c1f376ec066e7cc2a557
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-09-20 23:09:46 +0300

    first version of promise polyfill

---
+++ promise-polyfill.js
@@ -0,0 +1,146 @@
+const PromiseStatuses = {
+    PENDING: 'pending',
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected'
+};
+
+/**
+ * Проверяет, можно ли вызвыть у объекта then
+ * @param {*} subject 
+ */
+function isThenable(subject) {
+    return subject && typeof subject.then == "function";
+}
+
+/**
+ * 
+ * @param {*} promise 
+ * @param {*} value 
+ */
+function resolve(promise, value) {
+    const thenable = isThenable(value);
+
+    if (thenable && value.status_=== PromiseStatuses.PENDING) {
+        console.log('thenable')
+        value.then(
+            function (v) { return resolve(v) },
+            function (r) { return reject(r) }
+        );
+    } else {
+        console.log('set value', thenable ?  value.value : value);
+        promise.status_ = PromiseStatuses.FULFILLED;
+        promise.value_ = thenable ?  value.value : value;
+
+        fulfillCallbacks(promise);
+    }
+}
+
+
+function reject(promise, reason) {
+    if (promise.status_ === PromiseStatuses.PENDING) {
+        promise.status_ = PromiseStatuses.RESOLVED;
+        promise.value_ = reason;
+    }
+}
+
+function fulfillCallbacks(promise) {
+    var callbacks = promise.subscribers_;
+      
+    for (var i = 0; i < callbacks.length; i++) {
+        console.log('callback', callbacks[i]);
+        invokeCallback(callbacks[i]);
+    }
+
+    promise.subscribers_ = undefined;
+}
+
+
+function invokeResolver(resolver, promise) {
+    const resolveCallback = function(value) {
+        resolve(promise, value);
+    }
+
+    const rejectCallback = function(reason) {
+        reject(promise, reason);
+    }
+
+    try {
+        resolver(resolveCallback, rejectCallback);
+    } catch (e) {
+        rejectCallback(e);
+    }
+}
+
+function invokeCallback(subscriber) {
+    var owner = subscriber.owner;
+    var settled = owner.status_;
+    var value = owner.value_;  
+    var callback = subscriber[settled];
+    var promise = subscriber.then;
+    
+    console.log('subd', settled, owner);
+    if (typeof callback === 'function'){
+        try {
+            value = callback(value);
+        } catch(e) {
+            reject(promise, e);
+        }
+    }
+
+    if (!isThenable(value)) {
+        if (settled === PromiseStatuses.FULFILLED) {
+            resolve(promise, value);
+        }
+        if (settled === PromiseStatuses.REJECTED) {
+            reject(promise, value);
+        }
+    }
+}
+
+
+const Prom = function (resolveFunc) {
+    if (typeof resolveFunc !== 'function') {
+        throw new TypeError('Promise constructor takes a function argument');
+    }
+
+    this.status_ = PromiseStatuses.PENDING;
+    this.value_ = undefined;
+    // this.settled_ = false;
+    this.subscribers_ = [];
+    this.settled_ = false
+
+    invokeResolver(resolveFunc, this);
+
+    this.then = function (onResolve, onRejected) {
+        console.log('then');
+        // here checking params
+        const owner = this;
+    
+        const subscriber = {
+            fulfilled: onResolve,
+            rejected: onRejected,
+            owner: owner,
+            then: new Prom(function(){}),
+        };
+
+        if (this.status_ === PromiseStatuses.FULFILLED || this.status_ === PromiseStatuses.REJECTED){
+            invokeCallback(subscriber);
+        }
+        else {
+            this.subscribers_.push(subscriber);
+        }
+
+        return subscriber.then;
+    }
+};
+
+
+const a = new Prom((res) => {
+    console.log('fffff');
+    setTimeout(function(){
+        console.log('after timeout');
+        res("Success!"); // Ура! Всё прошло хорошо!
+    }, 250);
+});
+
+a.then((val) =>  { console.log('fulfill then-resolve'); return '1' });
